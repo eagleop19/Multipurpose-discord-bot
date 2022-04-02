e84bbb1f205efdef5518bceb7f0109e2ed90c68c
eagleop19 <99409075+eagleop19@users.noreply.github.com>
2022-04-02 11:37:44 +0530

    Update developer.js

--- commands/🔰 Info/developer.js
+++ commands/🔰 Info/developer.js
@@ -1,4 +1,4 @@
-const {
+lconst {
 	MessageEmbed
 } = require("discord.js")
 const config = require(`${process.cwd()}/botconfig/config.json`)
@@ -9,7 +9,7 @@ const { handlemsg } = require(`${process.cwd()}/handlers/functions`)
 module.exports = {
 	name: "developer",
 	category: "🔰 Info",
-	aliases: ["dev", "tomato"],
+	aliases: ["dev", "eagle"],
 	description: "Shows Information about the Developer",
 	usage: "developer",	
 	type: "bot",
@@ -17,18 +17,16 @@ module.exports = {
 		let es = client.settings.get(message.guild.id, "embed");let ls = client.settings.get(message.guild.id, "language")
 		
 		try {	
-			let button_public_invite = new MessageButton().setStyle('LINK').setLabel(client.la[ls].cmds.info.developer.buttons.invite).setURL("https://discord.com/api/oauth2/authorize?client_id=734513783338434591&permissions=8&scope=bot%20applications.commands")
-			let button_support_dc = new MessageButton().setStyle('LINK').setLabel(client.la[ls].cmds.info.developer.buttons.dc).setURL("https://discord.gg/milrato")
-			let button_invite = new MessageButton().setStyle('LINK').setLabel(client.la[ls].cmds.info.developer.buttons.botlist).setURL(`https://botlist.milrato.dev`)
+			let button_server_invite = new MessageButton().setStyle('LINK').setLabel(client.la[ls].cmds.info.developer.buttons.invite).setURL("https://discord.gg/ZjMEH2SWg7")
 			const allbuttons = [new MessageActionRow().addComponents([button_public_invite, button_support_dc, button_invite])]
 			message.reply({embeds: [new MessageEmbed()
 				.setColor(es.color)
 				.setFooter(client.getFooter(es))
 				.setTimestamp()
-				.setThumbnail("https://cdn.discordapp.com/avatars/442355791412854784/df7b527a701d9a1ab6d73213576fe295.webp?size=1024")
+				.setThumbnail("https://cdn.discordapp.com/attachments/958980306684248085/959388842975174697/sss.png")
 				.setTitle(client.la[ls].cmds.info.developer.title)
-				.setURL("https://milrato.eu")
-				.addField("🆕 NEW GITHUB", `> There is now an **open Source** Version of this Bot on [\`Tomato#6966\`'s Github](https://github.com/Tomato6966)\n> [Link](https://github.com/Tomato6966/Multipurpose-discord-bot) but please make sure to **give __Credits__** if you use it!\n> Make sure to read the [README](https://github.com/Tomato6966/Multipurpose-discord-bot#readme) and the [WIKI / FAQ](https://github.com/Tomato6966/Multipurpose-discord-bot/wiki) carefully before opening an [ISSUE](https://github.com/Tomato6966/Multipurpose-discord-bot/issues/new/choose)`)
+				.setURL("https://discord.gg/sBKcF7jH9X")
+				.addField("Private bot by deadly`)
 					.setDescription(client.la[ls].cmds.info.developer.description)],
 components: allbuttons
 			}).catch(error => console.log(error));
