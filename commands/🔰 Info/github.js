142521f0e21abc571335aaf7f39cd1b452c0d249
eagleop19 <99409075+eagleop19@users.noreply.github.com>
2022-04-02 13:04:03 +0530

    Update github.js

--- commands/🔰 Info/github.js
+++ commands/🔰 Info/github.js
@@ -17,9 +17,9 @@ module.exports = {
 		let es = client.settings.get(message.guild.id, "embed");let ls = client.settings.get(message.guild.id, "language")
 		
 		try {	
-			let button_public_invite = new MessageButton().setStyle('LINK').setLabel(client.la[ls].cmds.info.github?.buttons.invite).setURL("https://discord.com/api/oauth2/authorize?client_id=734513783338434591&permissions=8&scope=bot%20applications.commands")
-			let button_support_dc = new MessageButton().setStyle('LINK').setLabel(client.la[ls].cmds.info.github?.buttons.dc).setURL("https://discord.gg/milrato")
-			let button_invite = new MessageButton().setStyle('LINK').setLabel(client.la[ls].cmds.info.github?.buttons.botlist).setURL(`https://botlist.milrato.eu`)
+			let button_public_invite = new MessageButton().setStyle('LINK').setLabel(client.la[ls].cmds.info.github?.buttons.invite).setURL("https://discord.com/oauth2/authorize?client_id=954232308876062760&permissions=139858406496&scope=bot%20applications.commands")
+			let button_support_dc = new MessageButton().setStyle('LINK').setLabel(client.la[ls].cmds.info.github?.buttons.dc).setURL("https://discord.gg/sBKcF7jH9X")
+			let button_invite = new MessageButton().setStyle('LINK').setLabel(client.la[ls].cmds.info.github?.buttons.botlist).setURL(`https://youtube.com/c/REFLEXESPORTSINDIA`)
 			const allbuttons = [new MessageActionRow().addComponents([button_public_invite, button_support_dc, button_invite])]
 			message.reply({embeds: [new MessageEmbed()
 				.setColor(es.color)
